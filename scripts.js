9beda77ab0fe79aa5a2b33e6db2dadfa37f30d1f
RiderDeck-8 <153536747+RiderDeck-8@users.noreply.github.com>
2024-07-23 09:15:30 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Añadir cualquier interacción que desees en tu página
+    console.log('Curriculum vitae interactivo cargado');
+});
